037aad53124c72d536fb74d63e8736b43a2b3dbc
Koyeb App <koyebapp@gmail.com>
2024-12-20 03:46:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const favicon = require('serve-favicon');
-const secure = require('ssl-express-www');
-const path = require('path');
-const { color } = require('./lib/color.js');
+__path = process.cwd();
+var favicon = require('serve-favicon');
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
 
-const mainrouter = require('./routes/main');
-const apirouter = require('./routes/api');
+var mainrouter = require('./routes/main'),
+    apirouter = require('./routes/api')
 
-const PORT = process.env.PORT || 8080 || 5000 || 3000;
-
-const app = express();
-
-// Middleware setup
+var app = express()
 app.enable('trust proxy');
-app.set("json spaces", 2);
-app.use(cors());
-app.use(secure);
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(favicon(__path +'/views/favicon.ico'))
+app.use(express.static("public"))
 
-// Serve favicon
-app.use(favicon(path.join(__dirname, 'views', 'favicon.ico')));
-
-// Static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Route setup
 app.use('/', mainrouter);
 app.use('/api', apirouter);
 
-// Serve the index.html page when the root route is accessed
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  console.log(path.join(__dirname, 'views', 'index.html'));
-
-  
-});
-
-// Serve the docs.html page when "/docs" route is accessed
-app.get('/docs', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'docs.html'));
-  console.log(path.join(__dirname, 'views', 'docs.html'));
-});
-
-// Custom 404 Error Handler
-app.use((req, res) => {
-  res.status(404)
-     .set("Content-Type", "text/html")
-     .sendFile(path.join(__dirname, 'views', '404.html')); // Ensure correct path to 404.html
-console.log(path.join(__dirname, 'views', '404.html'));
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(color(`Server running on port ${PORT}`, 'green'));
-});
+    console.log(color("Server running on port " + PORT,'green'))
+})
 
-module.exports = app;
+module.exports = app
